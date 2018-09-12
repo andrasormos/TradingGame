046769e1c1b8eb27c69f499a00111640587455be
andras <andrasormos@gmail.com>
2018-09-12 09:04:33 +0100

    latest

---
+++ Visualize_GameEngine.py
@@ -0,0 +1,39 @@
+import time
+import datetime
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from GameEngine_v001 import PlayGame
+
+desired_width = 320
+pd.set_option('display.width', desired_width)
+
+np.set_printoptions(threshold=np.nan, linewidth=300)
+
+
+GE = PlayGame()
+#GE.startGame()
+df_segment = GE.getChartData()
+
+print(df_segment)
+print("\n")
+#df_segment.reset_index()
+
+
+
+# PLOT
+if 1 == 2:
+    # graphed on matrix
+    plt.imshow(df_segment, cmap='hot')
+    plt.show()
+
+
+    #ax2 = fig.add_subplot(211)
+    #plt.imshow(blank_matrix, cmap='hot')
+
+    #plt.show())
+
+
+
+
+
