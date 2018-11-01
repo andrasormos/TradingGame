a38d8605a0f739acee7b2b888c1c4e48c9e2ebcb
andras <andrasormos@gmail.com>
2018-11-01 21:41:04 +0000

    EXTRACTION

--- data_treatment/extract_historical3.py
+++ data_treatment/extract_historical3.py
@@ -40,9 +40,10 @@ def convert_unix_to_iso(t):
 
 def extractBTC(startDate, endDate):
     global BTCcnt
+    global extracted_unix_list
 
     output = client.get_historic_rates(gdax.BTC_GBP, startDate, endDate, granularity=900)
-    print(output)
+    #print(output)
     output = np.asarray(output)
 
     date = output[:, [0]]
@@ -56,6 +57,9 @@ def extractBTC(startDate, endDate):
         df_volume_column = int(volume[i])
 
         df_BTC.loc[BTCcnt] = df_date_column, df_close_column, df_volume_column
+        print(str(int(date[i])))
+        date_unix = str(int(date[i]))
+        extracted_unix_list.append(date_unix)
         #df_BTC.to_csv("./cryptoExtract/treatment/raw_BTC_GBP.csv", header=True, index=True)
         BTCcnt += 1
 
@@ -68,74 +72,62 @@ def extractBTC(startDate, endDate):
 # 2629744	1 month (30.4369 days)
 # 31556926	1 year (365.2422 days)
 
-# extracts 15 minutes increments (900)
-# go through data and check if an increment is missing, if missing, just copy previous price or mean between next
-
-# FOR THIS KEEP THE DATE IN UNIX!!!!!!
-
-# save 15min data into csv
-# save 60 min data into csv
-
 
 # give human iso timeframe
-end_date = "2018-11-01T07:00:00.000Z"
+end_date = "2018-10-29T07:00:00.000Z"
 end_date_unix = convert_iso_to_unix(end_date)
 
-# how many weeks would you like it to go back?
-iterations = 10
+# how many 300 batches would you like it to go back?
+batches = 10
+# 1 batch = 300 elements, so in total we have to end yp with 3000 elements
 
-last300_list = []
-last300_ISO_list = []
-last300 = end_date_unix
-last300_ISO = convert_unix_to_iso(end_date_unix)
+batch_unix_list = []
+batch_ISO_list = []
+batch_unix = end_date_unix
+batch_ISO = convert_unix_to_iso(end_date_unix)
 
-for i in range(iterations):
-    last300_list.append(last300)
-    last300_ISO = convert_unix_to_iso(last300)
-    last300_ISO_list.append(last300_ISO)
-    last300 -= 270000
+# CREATE LIST OF ISO DATES 270000 SECONDS APART (3 DAYS APART)
+for i in range(batches):
+    batch_unix_list.append(batch_unix)
+    batch_ISO = convert_unix_to_iso(batch_unix)
+    batch_ISO_list.append(batch_ISO)
+    batch_unix -= 270000  # 3 days # 270000 = 900×300
 
-#print(last300_list)
+# CREATE LIST OF 3000 ELEMENTS THAT WILL BE USED TO FIND MISSING ONES IN THE ACTUAL EXTRACTED DATA
+compare_unix_list = []
+compare_unix = end_date_unix
+for i in range(2999):
+    compare_unix_list.append(compare_unix)
+    compare_unix -= 900
 
-for i in range(len(last300_ISO_list)-1):
-    print("Iteration:", i)
-    endDate = last300_ISO_list[i]
-    startDate = last300_ISO_list[i+1]
+print("compare_unix_list:", len(compare_unix_list))
+print(compare_unix_list)
+print("\n")
+
+extracted_unix_list = []
+
+# QUERY ACTUAL DATA FROM GDAX
+for i in range(len(batch_ISO_list)-1):
+    print("Batch:", i)
+    endDate = batch_ISO_list[i]
+    startDate = batch_ISO_list[i+1]
 
     df_BTC = extractBTC(startDate, endDate)
-    print("sleep")
     sleep(0.4)
+
+print("extracted_unix_list:", len(extracted_unix_list))
+print(extracted_unix_list)
+print("\n")
+
+# NOW ITERATE THROUGH THE COMPARE LIST AND COMPARE IT TO THE EXTRACTED DATA AND FILL IN THE MISSING ONES
+# IT LOOKS LIKE YOU HAVE TO FILL IN LIKE 310 VALUES AT THE MOMENT
 
     #df_ETH = extractETH(startDate, endDate)
     #print(df_BTC)
     #print(df_ETH)
 
 
-
+# SAVE THE DF TO A CSV, THIS IS A VERY EFFICIENT METHOD FOR SAVING LOGS AS WELL
 df_BTC.to_csv("./cryptoExtract/treatment/raw_BTC_GBP.csv", header=True, index=True)
 
-# ---------------------------------------------------------------------------------------------------------------------
-# df = pd.read_csv("./cryptoExtract/treatment/raw_BTC_GBP.csv", index_col=0)
-# print("total:", len(df))
-# eval_df = df[0:3000]
-# eval_df.to_csv("./cryptoExtract/trainEval/eval_BTC_GBP.csv", index=True)
-# print(eval_df)
-#
-# df = pd.read_csv("./cryptoExtract/treatment/raw_BTC_GBP.csv", index_col=0)
-# train_df = df[3000:].reset_index(drop=True)
-# train_df.to_csv("./cryptoExtract/trainEval/train_BTC_GBP.csv", index=True)
-# print(train_df)
-#
-#
-#
-#
-# df = pd.read_csv("./cryptoExtract/treatment/raw_ETH_USD.csv", index_col=0)
-# print("total:", len(df))
-# eval_df = df[0:3000]
-# eval_df.to_csv("./cryptoExtract/trainEval/eval_ETH_USD.csv", index=True)
-# print(eval_df)
-#
-# df = pd.read_csv("./cryptoExtract/treatment/raw_ETH_USD.csv", index_col=0)
-# train_df = df[3000:].reset_index(drop=True)
-# train_df.to_csv("./cryptoExtract/trainEval/train_ETH_USD.csv", index=True)
-# print(train_df)
+
