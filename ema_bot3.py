498c3430e6e69ab00a726e39c4e1dcf9222dada6
andras <andrasormos@gmail.com>
2018-11-07 15:27:36 +0000

    EXTRACTION

--- ema_bot3.py
+++ ema_bot3.py
@@ -9,20 +9,22 @@ from matplotlib.widgets import Button, TextBox
 import os.path
 import sys
 from skimage import draw
+from pyti.exponential_moving_average import exponential_moving_average as ema
+
 plt.style.use('seaborn-darkgrid')
 
 df_BTC = pd.read_csv("./cryptoExtract/raw_BTC_GBP.csv", index_col=0)
 df_BTC = df_BTC.iloc[::-1].reset_index(drop=True)
 
 # SMA
-sma_days_A = 20
-sma_days_B = 2
+sma_days_A = 14
+sma_days_B = 1
 
 # START DATE
-btc_state_size = 1 # 49000, 62000
-start_date = 33000
+btc_state_size = 1 # 33000, 63000
+start_date = 40000
 end_date = start_date + btc_state_size
-game_end_date = 63000
+game_end_date = 62000
 
 # WALLET FINANCES
 fiat_cash_balance = 10000
@@ -46,8 +48,14 @@ buy_enabled = True
 sell_enabled = True
 
 
-df_BTC["Sma"] = df_BTC["Close"].ewm(span=ma_a, min_periods=0, adjust=False, ignore_na=False).mean()
-df_BTC["Ema"] = df_BTC["Close"].ewm(span=ma_b, min_periods=0, adjust=False, ignore_na=False).mean()
+# df_BTC["Sma"] = df_BTC["Close"].rolling(window=ma_a, min_periods=0).mean()
+# df_BTC["Ema"] = df_BTC["Close"].rolling(window=ma_b, min_periods=0).mean()
+								 #.ewm(span=200,adjust=False, min_periods=200-1).mean()
+# df_BTC["Sma"] = df_BTC["Close"].ewm(span=ma_a, min_periods=0, adjust=False, ignore_na=False).mean()
+# df_BTC["Ema"] = df_BTC["Close"].ewm(span=ma_b, min_periods=0, adjust=False, ignore_na=False).mean()
+
+df_BTC["Sma"] = df_BTC["Close"].ewm(span=ma_a, min_periods=200-1, adjust=False, ignore_na=False).mean()
+df_BTC["Ema"] = df_BTC["Close"].ewm(span=ma_b, min_periods=200-1, adjust=False, ignore_na=False).mean()
 
 if 1 == 2:
 	fig = plt.figure(figsize=(12, 10))
@@ -58,8 +66,8 @@ if 1 == 2:
 	ax1.legend()
 	plt.show()
 
-
 #'''
+
 for i in range(start_date, game_end_date):
 	if i % 10000 == 0:
 		print("STEP: ", i, "DATE: ", df_BTC["Date"][i])
@@ -75,20 +83,20 @@ for i in range(start_date, game_end_date):
 
 	buy = 0
 	sell = 0
-	# if days_since_buy > 2880:
-	# 	buy_enabled = True
+	if days_since_buy > 2880:
+		buy_enabled = True
 
 
 	# BUY
 	if btc_state_ma_b_price_current > btc_state_ma_a_price_current and buy_enabled == True:  #and days_since_buy > 2880:
 		days_since_buy = 0
-		btc_balance = trade_amount / btc_price_current
+		btc_balance += trade_amount / btc_price_current
 		fiat_cash_balance -= trade_amount
 		buy_enabled = False
 		sell_enabled = True
-
+		fullBalance = fiat_cash_balance + btc_balance * btc_price_current
 		print(df_BTC["Date"][i], " Price:", btc_price_current)
-		print("		BUY BTC NOW")
+		print("						BUY BTC NOW", "Full Balance: ", fullBalance, "BTC:", btc_balance)
 		#print("\n")
 		buy = btc_price_current
 
@@ -96,12 +104,12 @@ for i in range(start_date, game_end_date):
 	if btc_state_ma_b_price_current < btc_state_ma_a_price_current and btc_balance != 0:
 		fiat_cash_balance += btc_balance * btc_price_current
 		btc_balance = 0
+		days_since_buy = 0
 		buy_enabled = True
 		sell_enabled = False
-
+		fullBalance = fiat_cash_balance + btc_balance * btc_price_current
 		print(df_BTC["Date"][i], " Price:", btc_price_current)
-		print("		SELL BTC NOW")
-		print("Fiat Balance:", fiat_cash_balance)
+		print("						SELL BTC NOW", "Full Balance:", fullBalance)
 		#print("\n")
 		sell = btc_price_current
 
@@ -131,7 +139,7 @@ new_sell_list = zero_to_nan(sell_list)
 
 
 
-fig = plt.figure(figsize=(12, 10))
+fig = plt.figure(figsize=(19, 10))
 ax1 = fig.add_subplot(111)
 
 ax1.plot(price_list, "-", color='b', linewidth=1)
--- ema_bot4.py
+++ ema_bot4.py
@@ -9,20 +9,22 @@ from matplotlib.widgets import Button, TextBox
 import os.path
 import sys
 from skimage import draw
+import talib
+
 plt.style.use('seaborn-darkgrid')
 
 df_BTC = pd.read_csv("./cryptoExtract/raw_BTC_GBP.csv", index_col=0)
 df_BTC = df_BTC.iloc[::-1].reset_index(drop=True)
 
 # SMA
-sma_days_A = 20
+sma_days_A = 14
 sma_days_B = 2
 
 # START DATE
-btc_state_size = 1 # 49000, 62000
-start_date = 49000
+btc_state_size = 1 # 33000, 63000
+start_date = 30000
 end_date = start_date + btc_state_size
-game_end_date = 62000
+game_end_date = 45000
 
 # WALLET FINANCES
 fiat_cash_balance = 10000
@@ -45,75 +47,71 @@ sell_list = []
 buy_enabled = True
 sell_enabled = True
 
-btc_state = df_BTC.loc[start_date:end_date]
-btc_state_ma_a = df_BTC.loc[(start_date - ma_a):end_date]
-btc_state_ma_b = df_BTC.loc[(start_date - ma_b):end_date]
 
-for i in range(end_date, (game_end_date)):
-	if i % 500 == 0:
-		print("STEP: ", i, "DATE: ", btc_state["Date"][i])
+# df_BTC["Sma"] = df_BTC["Close"].rolling(window=ma_a, min_periods=0).mean()
+# df_BTC["Ema"] = df_BTC["Close"].rolling(window=ma_b, min_periods=0).mean()
 
-	# ADD NEXT BTC ROW TO STATE AND DELETE PREVIOUS
-	next_state_row = df_BTC.loc[[i+1]]
+# df_BTC["Sma"] = df_BTC["Close"].ewm(span=ma_a, min_periods=0, adjust=False, ignore_na=False).mean()
+# df_BTC["Ema"] = df_BTC["Close"].ewm(span=ma_b, min_periods=0, adjust=False, ignore_na=False).mean()
 
-	btc_state = pd.concat([btc_state, next_state_row])
-	btc_state = btc_state.drop(btc_state.index[0])
-	btc_price_current = btc_state["Close"].iloc[-1]
+df_BTC["Sma"] = talib.EMA(df_BTC["Close"], timeperiod=ma_a)
+df_BTC["Ema"] = talib.DEMA(df_BTC["Close"], timeperiod=ma_b)
+
+if 1 == 2:
+	fig = plt.figure(figsize=(19, 10))
+	ax1 = fig.add_subplot(111)
+	ax1.plot(df_BTC["Close"], "-", color='b', linewidth=1)
+	ax1.plot(df_BTC["Sma"], "-", color='r', linewidth=2, label=("sma" + str(sma_days_A)))
+	ax1.plot(df_BTC["Ema"], "-", color='purple', linewidth=2, label=("ema" + str(sma_days_B)))
+	ax1.legend()
+
+	plt.show()
+
+#'''
+
+for i in range(start_date, game_end_date):
+	if i % 10000 == 0:
+		print("STEP: ", i, "DATE: ", df_BTC["Date"][i])
+
+	btc_price_current = df_BTC["Close"][i]
 
 	# ADD NEXT SMA ROW
-	btc_state_ma_a = pd.concat([btc_state_ma_a, next_state_row])  # add to last row
-	btc_state_ma_a = btc_state_ma_a.drop(btc_state_ma_a.index[0])  # delete first row
-	btc_state_ma_a_applied = btc_state_ma_a["Close"].rolling(window=ma_a, min_periods=0).mean()  # apply average
-	btc_state_ma_a_applied = btc_state_ma_a_applied.loc[btc_state.index[0]:]  # crop the beginning using btc state's range
-	btc_state_ma_a_price_current = btc_state_ma_a_applied.iloc[-1]
+	btc_state_ma_a_price_current = df_BTC["Sma"][i]
 
-	# ADD NEXT EMA ROW
-	btc_state_ma_b = pd.concat([btc_state_ma_b, next_state_row])
-	btc_state_ma_b = btc_state_ma_b.drop(btc_state_ma_b.index[0])
-	btc_state_ma_b_applied = btc_state_ma_b["Close"].ewm(span=ma_b, min_periods=0, adjust=False, ignore_na=False).mean()
-	btc_state_ma_b_applied = btc_state_ma_b_applied.loc[btc_state.index[0]:]  # crop the beginning using btc state's range
-	btc_state_ma_b_price_current = btc_state_ma_b_applied.iloc[-1]
+	# ADD NEXT SMA ROW
+	btc_state_ma_b_price_current = df_BTC["Ema"][i]
 
-
-	if 1==2:
-		fig = plt.figure(figsize=(12, 10))
-		ax1 = fig.add_subplot(111)
-		ax1.plot(btc_state["Close"], "-", color='b', linewidth=2)
-		ax1.plot(btc_state_ma_a_applied, "-", color='r', linewidth=2, label=("sma" + str(sma_days_A)))
-		ax1.plot(btc_state_ma_b_applied, "-", color='purple', linewidth=2, label=("ema" + str(sma_days_B)))
-		ax1.legend()
-		plt.show()
 
 	buy = 0
 	sell = 0
-	if days_since_buy > 2880:
-		buy_enabled = True
+	# if days_since_buy > 1344:
+	# 	buy_enabled = True
 
 
 	# BUY
 	if btc_state_ma_b_price_current > btc_state_ma_a_price_current and buy_enabled == True:  #and days_since_buy > 2880:
 		days_since_buy = 0
-		btc_balance = trade_amount / btc_price_current
+		btc_balance += trade_amount / btc_price_current
 		fiat_cash_balance -= trade_amount
 		buy_enabled = False
 		sell_enabled = True
-
+		fullBalance = fiat_cash_balance + btc_balance * btc_price_current
 		print(df_BTC["Date"][i], " Price:", btc_price_current)
-		print("		BUY BTC NOW")
-		print("\n")
+		print("						BUY BTC NOW", "Full Balance: ", fullBalance, "BTC:", btc_balance)
+		#print("\n")
 		buy = btc_price_current
 
 	# SELL
 	if btc_state_ma_b_price_current < btc_state_ma_a_price_current and btc_balance != 0:
 		fiat_cash_balance += btc_balance * btc_price_current
 		btc_balance = 0
+		days_since_buy = 0
 		buy_enabled = True
 		sell_enabled = False
-
+		fullBalance = fiat_cash_balance + btc_balance * btc_price_current
 		print(df_BTC["Date"][i], " Price:", btc_price_current)
-		print("		SELL BTC NOW")
-		print("Fiat Balance:", fiat_cash_balance)
-		print("\n")
+		print("						SELL BTC NOW", "Full Balance:", fullBalance)
+		#print("\n")
 		sell = btc_price_current
 
 	days_since_buy += 1
@@ -142,7 +140,7 @@ new_sell_list = zero_to_nan(sell_list)
 
 
 
-fig = plt.figure(figsize=(12, 10))
+fig = plt.figure(figsize=(19, 10))
 ax1 = fig.add_subplot(111)
 
 ax1.plot(price_list, "-", color='b', linewidth=1)
@@ -154,16 +152,16 @@ ax1.plot(new_sell_list, "*", color='r', markersize=8)
 xi = [i for i in range(0, len(new_sell_list))]
 for i, txt in enumerate(profit_list):
 	if txt > 0:
-		ax1.annotate(txt, (xi[i], new_sell_list[i]), size=20, fontweight='bold', color='green')
+		ax1.annotate(txt, (xi[i], new_sell_list[i]), size=10, fontweight='bold', color='green')
 	else:
-		ax1.annotate(txt, (xi[i], new_sell_list[i]), size=20, fontweight='bold', color='red')
+		ax1.annotate(txt, (xi[i], new_sell_list[i]), size=10, fontweight='bold', color='red')
 
 xp = [p for p in range(0, len(new_buy_list))]
 for p, txt in enumerate(profit_list):
 	if txt > 0:
-		ax1.annotate(txt, (xi[p], new_buy_list[p]), size=20, fontweight='bold', color='green')
+		ax1.annotate(txt, (xi[p], new_buy_list[p]), size=10, fontweight='bold', color='green')
 	else:
-		ax1.annotate(txt, (xi[p], new_buy_list[p]), size=20, fontweight='bold', color='red')
+		ax1.annotate(txt, (xi[p], new_buy_list[p]), size=10, fontweight='bold', color='red')
 
 ax1.legend()
 
